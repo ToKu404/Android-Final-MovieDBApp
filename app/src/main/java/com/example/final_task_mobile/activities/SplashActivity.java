a53f305cea1b51bc1eebc230c1667cd6b3e69617
toku404 <phantom26isn@gmail.com>
2021-06-10 02:34:18 -0400

    cc2 splash clean

--- app/src/main/java/com/example/final_task_mobile/activities/SplashActivity.java
+++ app/src/main/java/com/example/final_task_mobile/activities/SplashActivity.java
@@ -2,27 +2,20 @@ package com.example.final_task_mobile.activities;
 
 
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.example.final_task_mobile.R;
 
 public class SplashActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set layout of splash screen
         setContentView(R.layout.activity_splash);
+
+        //make delay 2s and intent to mainactivity after that
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -34,12 +27,12 @@ public class SplashActivity extends Activity {
         }, 2000);
     }
 
+    //animation on start and finish
     @Override
     public void startActivity(Intent intent) {
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         super.startActivity(intent);
     }
-
     @Override
     public void finish() {
         super.finish();
