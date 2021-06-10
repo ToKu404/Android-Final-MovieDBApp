56a816176a8ef9f84c4df564827b1d198b92e1f0
toku404 <phantom26isn@gmail.com>
2021-06-10 02:43:07 -0400

    cc3 main activity

--- app/src/main/java/com/example/final_task_mobile/activities/MainActivity.java
+++ app/src/main/java/com/example/final_task_mobile/activities/MainActivity.java
@@ -17,41 +17,47 @@ import com.example.final_task_mobile.fragments.TvShowFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    //instance widget
     private BottomNavigationView bottomNavigationItemView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationItemView = findViewById(R.id.bottom_nav);
 
-        if(getSupportActionBar()!=null){
-            ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#1A212F"));
-            getSupportActionBar().setBackgroundDrawable(colorDrawable);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setDisplayUseLogoEnabled(true);
-        }
+        //bottomnav settings
+        bottomNavigationItemView = findViewById(R.id.bottom_nav);
         bottomNavigationItemView.setOnNavigationItemSelectedListener(this);
         bottomNavigationItemView.setSelectedItemId(R.id.menu_item_tv);
 
+        //actionbar set colour
+        actionBarSetColor();
     }
 
+
+
+    //on navigation selected
     @Override
     public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+
+        //fragment setting and instance
         Fragment fragment = null;
         switch (item.getItemId()){
             case R.id.menu_item_movie:
-                setActionBarTitle(getString(R.string.item_movie));
+                actionBarSetTitle(getString(R.string.item_movie));
                 fragment = new MovieFragment();
                 break;
             case R.id.menu_item_tv:
-                setActionBarTitle(getString(R.string.item_tv_show));
+                actionBarSetTitle(getString(R.string.item_tv_show));
                 fragment = new TvShowFragment();
                 break;
             case R.id.menu_item_favorite:
-                setActionBarTitle(getString(R.string.item_favorite));
+                actionBarSetTitle(getString(R.string.item_favorite));
                 fragment = new FavoriteFragment();
                 break;
         }
+
+        //replace frame with fragment
         if(fragment != null){
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fl_main, fragment)
@@ -60,7 +66,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
         return true;
     }
-    private void setActionBarTitle(String title) {
+
+    //actionbar settings method
+    private void actionBarSetColor(){
+        if(getSupportActionBar()!=null){
+            ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#1A212F"));
+            getSupportActionBar().setBackgroundDrawable(colorDrawable);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+        }
+    }
+    private void actionBarSetTitle(String title) {
         title = title.replaceAll("\\s","");
         title += ".";
         String first = title.substring(0,title.length()/2);
@@ -68,5 +84,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         getSupportActionBar().setTitle(Html.fromHtml(first+"<font color=\"#2C83F5\">"+end+"</font>"));
     }
 
+    //nothing happen, for my disable my frame layout *JUST FOR UI*
     public void nothing_happen(View view) {}
 }
